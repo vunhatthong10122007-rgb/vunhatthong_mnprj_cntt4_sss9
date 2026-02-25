5e1d724fae5783e994c062d47c9b8416712428c1
vunhatthong10122007-rgb <vunhatthong10122007@gmail.com>
2026-02-25 07:21:29 +0700

    Add files via upload

---
+++ vunhatthong_mnprj_cntt4_sss9/index.js
@@ -0,0 +1,11 @@
+let order = ["đơn hàng A", "đơn hàng B", "đơn hàng C", "đơn hàng D", "đơn hàng E"];
+let revenues = [1500, 2800, 1200, -500, 3200];
+
+const newRever = revenues.some((p) => p<0);
+console.log(`có đơn hàng âm : ${newRever}`);
+const newOrder = order.every((o) => o>500);
+console.log(`tất cả trên 500 : ${newOrder}`);
+
+const netProfits = revenues.map((net) => net*0.9);
+console.log(`lợi nhuận ròng : ${netProfits}`);
+
